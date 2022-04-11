122cfb4b7dc709b69933dd3ff6ad725a744dff42
level237 <Bramslevel129@gmail.com>
2022-04-11 09:15:29 +0100

    database seed in laravel 9

--- database/migrations/2022_04_10_212940_create_orders_table.php
+++ database/migrations/2022_04_10_212940_create_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number');
             $table->foreignIdFor(User::class)
             ->constrained()
             ->onDelete('cascade');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Order;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        \App\Models\post::factory(10)->create();
+        \App\Models\Order::factory(25)->create();
+        // \App\Models\post::factory(10)->create();
     }
 }
